98871bfdfad3cfd36c16a1f4c8aca113efe97d12
oliverbaker10-max <oliverbaker10@icloud.com>
2026-03-27 23:57:41 +0000

    Update Serviceworker.js

--- Serviceworker.js
+++ Serviceworker.js
@@ -2,7 +2,7 @@ self.addEventListener("install", e => {
   e.waitUntil(
     caches.open("pokedex-cache").then(cache => {
       return cache.addAll([
-        "/",
+        "./",
         "index.html",
         "style.css",
         "script.js"
